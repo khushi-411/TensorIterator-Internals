e31a96a47717f29e8d7faa1c52204aed18b5a0b6
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-02-10 00:07:14 +0530

    add vectorized approach

---
+++ cases/vectorized_implementation.cpp
@@ -0,0 +1,32 @@
+// Sample Code
+
+namespace at {
+namespace native {
+namespace {
+static void lerp_kernel(
+    Tensor& ret,
+    const Tensor& self,
+    const Tensor& end,
+    Scalar weight) {
+  auto builder = at::TensorIterator::Builder();
+  builder.add_output(ret);
+  builder.add_input(self);
+  builder.add_input(end);
+  auto iter = builder.build();
+
+  AT_DISPATCH_FLOATING_TYPES(ret.scalar_type(), "lerp_kernel", [&] {
+    scalar_t weight_val = weight.to<scalar_t>();
+    auto weight_vec = Vec256<scalar_t>(weight_val);
+    auto reversed_weight_vec = Vec256<scalar_t>(1 - weight_val);
+    at::native::binary_kernel_vec(
+        *iter,
+        [=](scalar_t self_val, scalar_t end_val) {
+          return (weight_val < 0.5)
+              ? self_val + weight_val * (end_val - self_val)
+              : end_val - (end_val - self_val) * (1 - weight_val);
+        },
+        [=](Vec256<scalar_t> start_vec, Vec256<scalar_t> end_vec) {
+          return reversed_weight_vec * start_vec + weight_vec * end_vec;
+	 });
+  });
+}
